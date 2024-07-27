4ec301ae039f572975dd7e05806b16ea38d02717
rahul253040 <mdrahulsarkar5525@gmail.com>
2024-07-27 20:15:20 +0600

    update

---
+++ form/form_main.js
@@ -0,0 +1,11 @@
+const inputs = document.querySelectorAll('input'); 
+
+
+
+
+const proccedBtn = document.querySelector('.btn');
+const btnImg = document.querySelector('.btn-img');
+
+proccedBtn.addEventListener('click',()=>{
+    btnImg.classList.remove('pp');
+});
